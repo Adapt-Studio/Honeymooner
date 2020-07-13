3b0e6138b6fc55f9d08341b894331ea2cb6a1dec
mac <65713232+macdevelops@users.noreply.github.com>
2020-07-13 16:13:36 +0100

    fix: footer on about + menu toggle

--- evolve.js
+++ evolve.js
@@ -4,6 +4,5 @@ fetch('./components/header.html') .then(response => { return response.text() })
 .then(data => { document.querySelector('header').innerHTML = data; });
 fetch('./components/footer.html') .then(response => { return response.text() })
 .then(data => { document.querySelector('footer').innerHTML = data; }); function
-menu() { if (document.getElementById('dropdown').style.display = 'none;') {
-document.getElementById('menu-btn').style.boxShadow = 'none';
-document.getElementById('dropdown').style.display = 'flex'; } }
+menu() { document.getElementById('dropdown').classList.toggle('show');
+document.getElementById('menu-btn').classList.toggle('showBtn'); }
